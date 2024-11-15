257a01c6d9de9bb7e59691faa5155b755f8e8fce
Walisson Vilela <59664662+Walisson-Vilela@users.noreply.github.com>
2024-11-15 14:49:51 -0300

    Adição de mascara para input de placa
    Adição de mascara para input de placa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './_components/navbar/navbar.component';
 import { FooterComponent } from './_components/footer/footer.component';
 import { VehicleListComponent } from './_components/vehicle-list/vehicle-list.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 import { NgOptimizedImage } from '@angular/common';
 import { LoginComponent } from './pages/login/login.component';
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 
 @NgModule({
   declarations: [
@@ -25,8 +25,10 @@ import { LoginComponent } from './pages/login/login.component';
     AppRoutingModule,
     FormsModule,
     NgOptimizedImage,
+    NgxMaskDirective,
+    ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [provideNgxMask({ /* opções de cfg */ })],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
