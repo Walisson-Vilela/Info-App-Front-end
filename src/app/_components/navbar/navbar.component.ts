00cdbbaa7a7baec54920cf06435f50955e7edf92
Walisson Vilela <59664662+Walisson-Vilela@users.noreply.github.com>
2024-11-15 10:34:16 -0300

    Ajuste no botao para logout
    Ajuste no botao para logout

--- src/app/_components/navbar/navbar.component.ts
+++ src/app/_components/navbar/navbar.component.ts
@@ -1,12 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'; // Importa o Router
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+
+  constructor(private router: Router) { } // Injeta o Router
+
   ngOnInit(): void {
-      console.log("O navbar inicializou")
+    console.log("O navbar inicializou");
+  }
+
+  // Método para redirecionar ao clicar no ícone de logout
+  onLogout(): void {
+    this.router.navigate(['/login']); // Navega para a página de login
   }
 }
